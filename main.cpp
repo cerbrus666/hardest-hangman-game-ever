e03b579765359b640be2d8d2e56a784fb5ee4623
cerberus666 <cerberusofdeath@gmail.com>
2023-06-03 21:41:25 +0800

    declare guessed word variable

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@ int main() {
 	// Input word
 	char n[100];
 	const char guess_this_word[8] = "Hangman"; // Use char c-string
+	char guessed_word[8] = "_______";
 	std::cout << "Enter a character: ";
 	std::cin.getline(n, 50);
 	while (strlen(n) > 1) {
@@ -17,6 +18,17 @@ int main() {
 	int i;
 	for (i = 1; i < 10; ++i) {
 		std::cout << "Just have some fun";
+	// TODOS: 
 	}
 	
+	// If present, get the index in guess_this_word array
+	// 
+	// 
+	// 
+	// Think about case with multiple same characters in word
+	// 
+	// 
+	// 
+	// How to display the current status of guessed words?
+
 }
