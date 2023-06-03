4d6d86912809c9618624d03ed452400275df05f6
cerberus666 <cerberusofdeath@gmail.com>
2023-06-03 20:57:23 +0800

    basic input

--- main.cpp
+++ main.cpp
@@ -2,8 +2,13 @@
 #include <iostream>
 
 int main() {
-	char n;
-	char guess_this_word[8] = "Hangman";
+	char n[100];
+	char guess_this_word[8] = "Hangman"; // Use char c-string
+	int length_of_input;
 	std::cout << "Enter a character: ";
 	std::cin >> n;
+	length_of_input = strlen(n);
+	if (length_of_input > 1) {
+		std::cout << "You input more than a character. Please re-input";
+	}
 }
