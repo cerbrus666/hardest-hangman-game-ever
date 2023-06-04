b344b2dd51e4b382e659597169fc8a0f4dae2b23
cerberus666 <cerberusofdeath@gmail.com>
2023-06-04 09:59:05 +0800

    more message for saving and not saving

--- main.cpp
+++ main.cpp
@@ -2,10 +2,11 @@
 #include <iostream>
 
 int main() {
-	// Input word
+	// Input word & constants
 	char n[100];
-	const char guess_this_word[8] = "hangman"; // Use char c-string
+	const char guess_this_word[8] = "Hangman"; // Use char c-string
 	char guessed_word[8] = "_______";
+	int die = 0;
 	while(strcmp(guessed_word, guess_this_word) != 0){
 		std::cout << "The word is: " << guessed_word << std::endl;
 		std::cout << "Enter a character to guess: ";
@@ -28,25 +29,18 @@ int main() {
 		}
 		if (strcmp(before_guess, guessed_word) == 0) {
 			std::cout << "You are one step closer to die" << std::endl;
+			die++;
 		}
 		std::cout << "The characters guessed so far: " << guessed_word << std::endl;
+		if (die > 5) {
+			std::cout << "The guy was hanged" << std::endl;
+			std::cout << "You let the man died" << std::endl;
+			std::cout << "You're a murderer" << std::endl;
+			return 0;
+		}
+		if (strcmp(guessed_word, guess_this_word) == 0) {
+			std::cout << "You saved the guy" << std::endl;
+			std::cout << "You're a hero, congratulations" << std::endl;
+			return 0;
+		}
 	}
-	
-	
-	// If present, get the index in guess_this_word array
-	// for i=1, i < strlen(guessed_word), i++ , loop length of the word
-	// if guessed_word[i] == n
-	// array index_store, store current i
-	// get length of index_store, loop, and assign guessed_word[item in index_store]
-	//  
-	// 
-	// 
-	// Think about case with multiple same characters in word
-	// 
-	// 
-	// 
-	// How to display the current status of guessed words?
-	// print guessed_word?
-	
-
-}
